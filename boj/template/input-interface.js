f48ebc9c2b0976acb56be8cef2f411baaf1acc41
Jiyoung <janice10508@gmail.com>
2021-08-19 20:48:49 +0900

    Add: 백준 입출력 템플릿

--- boj/template/input-interface.js
+++ boj/template/input-interface.js
@@ -1,5 +1,4 @@
 // 입력이 한줄일때 
-
 // 1. 정수 하나인경우 
 let fs = require('fs');
 let input = Number(fs.readFileSync('dev/stdin'));
@@ -10,7 +9,6 @@ let input = fs.readFileSync('dev/stdin').toString().split(' ');
 /* ------------------------------------------------------------- */
 
 // 입력이 여러줄일때 
-
 // 1. 예제 데이터의 개수가 주어진 경우 
 let fs = require('fs');
 let input = fs.readFileSync('dev/stdin').toString().split('\n');
